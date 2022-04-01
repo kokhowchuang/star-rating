748730581b4a0e5bccfc031544718ea36979b915
kokhow <chuangkokhow@gmail.com>
2022-04-01 09:38:20 +0800

    Done average score and added socket.io client

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ import UserRating from "./components/user-rating";
 import Rating from "./components/rating";
 import { submitRating } from "./api/submitRating";
 import { getRating } from "./api/getRating";
-import { selectRatingList } from "./reducer/ratingSlice";
+import { selectRatingList, selectTotalScore } from "./reducer/ratingSlice";
+import { add } from "./reducer/ratingSlice";
 
 function App() {
   const dispatch = useDispatch();
   const rows = useSelector(selectRatingList);
+  const totalScore = useSelector(selectTotalScore);
+  const average = (totalScore / rows.length).toFixed(1);
   const [close, setClose] = useState(true);
   const [review, setReview] = useState("");
   const [rating, setRating] = useState(5);
+
+  const io = require("socket.io-client");
+  const socket = io("http://localhost:3000", { transports: ["websocket"] });
 
   useEffect(() => {
     dispatch(getRating());
@@ -23,10 +29,18 @@ function App() {
 
   const handleOpen = () => {
     setClose(!close);
+    setReview("");
+    setRating(5);
   };
 
   const handleSubmitRating = () => {
     dispatch(submitRating({ review, rating }));
+    socket.emit("rating", {
+      review,
+      rating,
+      productId: "6245b0414bb8b70aff189bc1",
+    });
+
     setClose(!close);
   };
 
@@ -34,13 +48,18 @@ function App() {
     setRating(score);
   };
 
+  socket.on("newRating", (data) => {
+    console.log("Message: ", data);
+    // dispatch(add({ payload: data }));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="review-header">
           <div className="title">The Minimalist Entrepreneur</div>
           <div className="review-header-wrapper">
-            <div>3.8</div>
+            <div>{rows.length > 0 && average}</div>
             <div>
               <button className="flat-button" onClick={handleOpen}>
                 Add Review
@@ -72,6 +91,7 @@ function App() {
               <Rating
                 precision={0.5}
                 clickable={true}
+                defaultValue={rating}
                 onChange={handleClickedRating}
               ></Rating>
             </span>
@@ -79,6 +99,7 @@ function App() {
             <textarea
               type="text"
               placeholder="Start typing ..."
+              value={review}
               onChange={(evt) => {
                 setReview(evt.target.value);
               }}
--- src/reducer/ratingSlice.js
+++ src/reducer/ratingSlice.js
@@ -4,12 +4,17 @@ export const ratingSlice = createSlice({
   name: "rating",
   initialState: {
     list: [],
+    totalScore: 0,
   },
   reducers: {
     add: (state, action) => {
+      state.totalScore += action.payload.score;
       state.list.push(action.payload);
     },
     fetch: (state, action) => {
+      action.payload.forEach((element) => {
+        state.totalScore += element.score;
+      });
       state.list = [...action.payload];
     },
     throwError: (state, action) => {
@@ -21,5 +26,6 @@ export const ratingSlice = createSlice({
 export const { add, fetch, throwError } = ratingSlice.actions;
 
 export const selectRatingList = (state) => state.rating.list;
+export const selectTotalScore = (state) => state.rating.totalScore;
 
 export default ratingSlice.reducer;
